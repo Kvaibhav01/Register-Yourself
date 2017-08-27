56362b871adf2aed8e328c33baee3687312dcc88
Vaibhav Khulbe <kvaibhav01@live.in>
2017-08-27 12:54:52 +0530

    Created Contact.java
    Initial commit

---
+++ app/src/main/java/com/example/kvaib/resgisteryourself/Contact.java
@@ -0,0 +1,53 @@
+package com.example.kvaib.resgisteryourself;
+
+/**
+ * Created by kvaib on 15-08-2017.
+ */
+
+public class Contact {
+
+
+    String name, email, uname, pass;
+
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getName()
+    {
+        return this.name;
+    }
+
+    public void setEmail (String email)
+    {
+        this.email = email;
+    }
+
+    public String getEmail()
+    {
+        return this.email;
+    }
+
+    public void setUname (String uname)
+    {
+        this.uname = uname;
+    }
+
+    public String getUname()
+    {
+        return this.uname;
+    }
+
+    public void setPass (String pass)
+    {
+        this.pass = pass;
+    }
+
+    public String getPass()
+    {
+        return this.pass;
+    }
+
+}
